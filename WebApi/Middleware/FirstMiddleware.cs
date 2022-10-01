1ef5b11f1bc0700116e873d94aa6351b4d29e2ba
ilkertulgar <i.tulgar@quasal.com.tr>
2022-10-01 13:01:06 +0300

    Middleware example

---
+++ WebApi/Middleware/FirstMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace WebApi.Middleware;
+
+public class FirstMiddleware
+{
+    private readonly RequestDelegate _next;
+    public FirstMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        Console.WriteLine("Hello World");
+        await _next.Invoke(context);
+        Console.WriteLine("By World");
+    }
+    static public class HelloMiddlewareExtension
+    {
+        public static IApplicationBuilder UseHello(IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<FirstMiddleware>();
+        }
+    }
+
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -30,6 +30,37 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+// Middleware işlemleri
+//app.Run(async contex=> Console.WriteLine("Middlware 1."));
+
+// app.Use(async (context, next) =>
+// {
+//     Console.WriteLine("Middleware 1 başladı.");
+//     await next.Invoke();
+//     Console.WriteLine("Middleware 1 sonlandırılıyor.");
+// });
+//
+// app.Use(async (context, next) =>
+// {
+//     Console.WriteLine("Middleware 2 başladı.");
+//     await next.Invoke();
+//     Console.WriteLine("Middleware 2 sonlandırılıyor.");
+// });
+//
+// app.Use(async (context, next) =>
+// {
+//     Console.WriteLine("Middleware 3 başladı.");
+//     await next.Invoke();
+//     Console.WriteLine("Middleware 3 sonlandırılıyor.");
+// });
+
+// app.Map("/example", interalApp => interalApp.Run(async context =>
+// {
+//     Console.WriteLine("/example middleware tetiklendi");
+//     await context.Response.WriteAsync("/example middleware tetiklendi");
+// }));
+
+//app.MapWhen(x => x.Request.Method == "GET", internalApp => { internalApp.Run(async context => { app.MapWhen(x => x.Request.Method == "GET", internalApp => { Console.WriteLine("MapWhen Tetiklendi."); }); }); });
 
 app.MapControllers();
 app.Run();
